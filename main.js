7e03f0fa827172319fe6908b9a6a25fcd291d181
Daniel Crego <dcrego90@gmail.com>
2017-09-19 23:36:12 +0200

    HTTPS server
    Added the HTTPS server.
    Renamed splash.html as public.html
    Created private.html
    Minor modifications in main,js

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-console.log(new Date(), 'Initializing server'); // https://nodejs.org/api/http.html
+console.log(new Date(), 'Initializing server');
 // Requires
-const fs = require('fs');
-const http = require('http');
-// Server
+const fs = require('fs');       // https://nodejs.org/api/fs.html
+const http = require('http');   // https://nodejs.org/api/http.html
+const https = require('https'); // https://nodejs.org/api/https.html
+const ssl = require('./modules/ssl');
+const config = require('./resources/json/config.json');
+// HTTP server
 http.createServer((req,res)=>{
-  fs.readFile('resources/html/splash.html', (err, data)=>{
+  fs.readFile('resources/html/public.html', (err, data)=>{
     if (err) {
       console.console.log(new Date(), err);
       res.writeHead(500, {'Content-Type': 'text/plain'});
@@ -14,6 +17,21 @@ http.createServer((req,res)=>{
       res.end(data);
     }
   });
-}).listen(80, ()=>{
-  console.log('Server running on port 80');
+}).listen(config.ports.http, ()=>{
+  console.log(new Date(), 'HTTP server running on port', config.ports.http);
+});
+// HTTPS server
+https.createServer(ssl.options, (req,res)=>{
+  fs.readFile('resources/html/private.html', (err, data)=>{
+    if (err) {
+      console.console.log(new Date(), err);
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.end('Error, please reload this page (F5) after a while. Thank you!');
+    } else {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(data);
+    }
+  });
+}).listen(443, ()=>{
+  console.log(new Date(), 'HTTPS server running on port', config.ports.https);
 });
