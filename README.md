db744ba4a3007b829ac1d0e33356eff7fd3d2185
Daniel Crego <dcrego90@gmail.com>
2017-09-19 22:13:06 +0200

    HTML splash
    The server now shows a splash page.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# celerity
+# Celerity
 A fast web app server.
+## Try it
+Celerity runs on node.js , you can find everything you need under https://nodejs.org/ .
+
+After installing node you just need to go to the root directory of this project and run `node ./main.js` or `node .\main.js` if you are running it on Windows.
--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 console.log(new Date(), 'Initializing server'); // https://nodejs.org/api/http.html
 // Requires
+const fs = require('fs');
 const http = require('http');
-// Constants
-const headers = {'Content-Type': 'text/plain'};
 // Server
 http.createServer((req,res)=>{
-  res.writeHead(200, headers);
-  res.write('Hello world!');
-  res.end();
-}).listen(80, function(){
+  fs.readFile('resources/html/splash.html', (err, data)=>{
+    if (err) {
+      console.console.log(new Date(), err);
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.end('Error, please reload this page (F5) after a while. Thank you!');
+    } else {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(data);
+    }
+  });
+}).listen(80, ()=>{
   console.log('Server running on port 80');
 });
