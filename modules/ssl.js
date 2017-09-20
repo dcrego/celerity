cd1e3b04e71b3fcf6e6d58b60b8fc1178267af88
Daniel Crego <dcrego90@gmail.com>
2017-09-20 21:32:40 +0200

    Clean up
    Moved resources/json/config.json to resources/ssl/config.json

--- modules/ssl.js
+++ modules/ssl.js
@@ -1,6 +1,6 @@
 // Requires
 const fs = require('fs');
-const config = require('../resources/json/config.json').sslConfig;
+const config = require('../resources/ssl/config.json').sslConfig;
 exports.options = {
   cert: fs.readFileSync(config.cert),
   key: fs.readFileSync(config.key),
