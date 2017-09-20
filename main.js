dedafd600013bb84fd91adcf0b31ab7d3f86dbbc
Daniel Crego <dcrego90@gmail.com>
2017-09-20 21:13:29 +0200

    Servers
    Migrated the responsibility of creating and managing the servers from main.js to modules/servers.js

--- main.js
+++ main.js
@@ -1,15 +1,8 @@
 console.log(new Date(), 'Initializing server');
 // Requires
-const http = require('http');   // https://nodejs.org/api/http.html
-const https = require('https'); // https://nodejs.org/api/https.html
-const ssl = require('./modules/ssl');
-const servers = require('./modules/servers');
-const config = require('./resources/json/config.json');
-// HTTP server
-http.createServer(servers.public.handleRequest).listen(config.ports.http, ()=>{
-  console.log(new Date(), 'HTTP server running on port', config.ports.http);
-});
-// HTTPS server
-https.createServer(ssl.options, servers.safe.handleRequest).listen(443, ()=>{
-  console.log(new Date(), 'HTTPS server running on port', config.ports.https);
-});
+const srv = require('./modules/servers');
+const config = require('./servers/index.json');
+// Servers
+for (var name in config) {
+  new srv.Server(name, config[name]).listen();
+}
--- modules/servers.js
+++ modules/servers.js
@@ -1,15 +1,24 @@
 // Requires
 const fs = require('fs');       // https://nodejs.org/api/fs.html
-const config = require('../servers/index.json');
+const ssl = require('./ssl');
 // Constants
 const ERROR_CODE = 500;
 const ERROR_HEADERS = {'Content-Type': 'text/plain'};
 const ERROR_BODY = 'Error, please reload this page (F5) after a while. Thank you!';
+const SERVER_FACTORIES = {
+  // protocol: factory
+  http: require('http').createServer // https://nodejs.org/api/http.html
+}
 // Functions
-function Server(name) {
+SERVER_FACTORIES.https = function (callback) {
+  return require('https').createServer(ssl.options, callback); // https://nodejs.org/api/https.html
+}
+exports.Server = function (name, config) {
   this.name = name;
+  this.protocol = config.protocol;
+  this.port = config.port;
   var content = require(`../servers/${this.name}/content.json`);
-  this.handleRequest = function (req, res) {
+  function handleRequest (req, res) {
     fs.readFile(content.bodyPath, (err, data)=>{
       if (err) {
         console.console.log(new Date(), err);
@@ -21,9 +30,12 @@ function Server(name) {
       }
     });
   }
-}
-var name;
-for (var id in config) {
-  name = config[id];
-  exports[name] = new Server(name);
+  this.listen = function (callback) {
+    if (!callback) {
+      callback = function () {
+        console.log(new Date(), config.protocol, 'server running on port', config.port);
+      }
+    }
+    SERVER_FACTORIES[config.protocol](handleRequest).listen(config.port, callback);
+  }
 }
